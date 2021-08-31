18f454e0d9139c8424bbbde0f3034e59a0bc68bd
J.D <2770323401@qq.com>
2021-09-01 03:33:31 +0800

    Delete some files useless

--- LoadModel/Camera.h
+++ LoadModel/Camera.h
@@ -69,7 +69,7 @@ public:
     // processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
     void ProcessKeyboard(Camera_Movement direction, float deltaTime)
     {
-        float velocity = MovementSpeed * deltaTime*10;
+        float velocity = MovementSpeed * deltaTime*5;
         if (direction == FORWARD)
             Position += Front * velocity;
         if (direction == BACKWARD)
